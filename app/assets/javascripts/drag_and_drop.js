389317452df1d8c35c5936c12daffef0d7a400dc
Laxmi Pattar <laxmipattar710@gmail.com>
2019-09-11 16:13:46 +0530

    Updated the droppable function

--- app/assets/javascripts/drag_and_drop.js
+++ app/assets/javascripts/drag_and_drop.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
 	enableDragAndDrop()
-})
+});
 
 var enableDragAndDrop = function() {
 	$( ".draggable" ).draggable();
 	$( ".droppable" ).droppable({
 		drop: function(event, ui) {
-			var card = $(ui.draggable);
-			var bucket = $(event.target);
-			
+			var card = $(ui.draggable).data('task-id');
+			var bucket = $(this).attr('id');
+
+			$.ajax({
+				method: 'put',
+				url: '/tasks/' + card,
+				dataType: 'script',
+				data: {task: { status: bucket} }
+
+			})
+		},
+
+		over: function( event, ui ) {
+			console.log('over')
 		}
 	});
 }
+
